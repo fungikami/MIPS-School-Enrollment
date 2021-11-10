04d93960a183c5180657dbfda8d0c057036dc9e2
chrischriscris <18-10892@usb.ve>
2021-11-09 23:28:53 -0400

    primero

--- Proy1-Fung-Gómez.s
+++ Proy1-Fung-Gómez.s
@@ -16,3 +16,18 @@ arcDef:	.asciiz "caso1-InsDefinitiva.txt"
 
 # Planificación de registros
 
+main:
+    # Abrir archivo
+    li $v0 13
+    lw $a0 arcEst
+    li $a1 0
+    syscall # Retorna $v0 (file descriptor)
+
+    # Verificar si el archivo existe
+    bltz $v0, fin
+    move $a0 $v0 # Guarda el file descriptor en $a0
+
+    # 
+
+fin:
+    # Abortar si no se pudo abrir el archivo
