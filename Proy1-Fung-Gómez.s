e617c13c6baac73c918a26e6be45ccf2c255b001
chrischriscris <18-10892@usb.ve>
2021-11-09 23:53:16 -0400

    comentarios 1

--- Proy1-Fung-Gómez.s
+++ Proy1-Fung-Gómez.s
@@ -27,7 +27,75 @@ main:
     bltz $v0, fin
     move $a0 $v0 # Guarda el file descriptor en $a0
 
-    # 
+    Estudiantes>.append(estudiante)
+
+    # ------------ MATERIAS ---------------
+
+    # Abrir archivo
+    li $v0 13
+    lw $a0 arcInst
+    li $a1 0
+    syscall # Retorna $v0 (file descriptor)
+
+    move $a0 $v0
+
+    # Verificar $v0 < 0 (too malo)
+    # Leer archivo ($v0=14) ($a0=$v0)
+    # Verificar $v0 < 0 (too malo)
+
+
+
+
+    # Por cada línea:
+        # Crear materia
+        # 7 chars:
+            # Guardar materia.codigo
+        # do while != “”:
+            # Guardar materia.nombre
+        # 1 chars:
+            # Guardar materia.creditos
+        # 3 chars:
+            # Guardar materia.cupos
+        # 3 chars:
+            # Guardar materia.creditos
+        
+        # <estructura Materia>.append(materia)
+
+    # ------------ SOLICITUDES ---------------
+
+    # Abrir archivo
+    li $v0 13
+    lw $a0 arcSol
+    li $a1 0
+    syscall # Retorna $v0 (file descriptor)
+
+    move $a0 $v0
+
+    # Verificar $v0 < 0 (too malo)
+    # Leer archivo ($v0=14) ($a0=$v0)
+    # Verificar $v0 < 0 (too malo)
+
+    # Por cada línea:
+        # Crear solicitud
+        # 8 chars:
+            # Guardar solicitud.carnet
+            # Hacer estudiante.numSolicitudes++
+    # 7 chars:
+            # Guardar solicitud.codigo
+            # Hacer estudiante.creditosInscribiendo++
+
+        # <estructura Solicitud>.append(solicitud)
+
+    # Procesar vainas
+
+    # Escribir archivo tentativo
+
+    # Abrir solicitudes de corrección
+
+    # Procesar vainas
+
+    # Escribir archivo definitivo
+
 
 fin:
     # Abortar si no se pudo abrir el archivo
